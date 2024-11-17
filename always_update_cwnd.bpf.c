681bc96adbb282b8d2a1530fbd1ed9ef7f43bb4c
hyouteki <lakshay21060@iiitd.ac.in>
2024-11-17 17:05:11 +0530

    add ssthresh map

--- always_update_cwnd.bpf.c
+++ always_update_cwnd.bpf.c
@@ -28,6 +28,13 @@ struct {
     __type(key, __u32);
     __type(value, __u32);
     __uint(max_entries, 1);
+} SSThreshMap SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, __u32);
+    __type(value, __u32);
+    __uint(max_entries, 1);
 } FallbackMap SEC(".maps");
 
 int verify_remote_addr(__u32 remote_ip4) {
@@ -62,7 +69,7 @@ int always_update_cwnd(struct bpf_sock_ops *skops) {
      * *user_wnd: cwnd for that client (previously stored by the user space code)
      * ret: return value
      */
-    int key = 0, val = 0, ret = 0, op, *userCwnd, *fallbackValue;
+    int key = 0, val = 0, ret = 0, op, *userCwnd, *fallbackValue, *userSSThresh;
 
     op = (int)skops->op;
     bpf_printk("log: skops opcode %d for src IP %pI4", op, &addr.s_addr);
@@ -73,7 +80,7 @@ int always_update_cwnd(struct bpf_sock_ops *skops) {
     if (!userCwnd) {
         // key not present in cwnd map
         ret = bpf_map_update_elem(&CwndMap, &key, &val, BPF_NOEXIST);
-        if (ret != 0) bpf_printk("log: cwnd value update to NOEXIST failed");
+        if (ret != 0) bpf_printk("log: CWND value update to NOEXIST failed");
         return 0;
     }
         
@@ -81,13 +88,34 @@ int always_update_cwnd(struct bpf_sock_ops *skops) {
     if (!*userCwnd) return 0;
     // *userCwnd = ceil(*userCwnd/MSS)
     int newCwnd = (unsigned long long)(*userCwnd+MSS-1)/(unsigned long long)MSS;
-    bpf_printk("log: changing CWND -> %d (%d MSS)", *userCwnd, newCwnd);
+    bpf_printk("log: updating CWND to %d (%d MSS)", *userCwnd, newCwnd);
     *userCwnd = newCwnd;
+
+    userSSThresh = bpf_map_lookup_elem(&SSThreshMap, &key);
+    if (!userSSThresh) {
+        // key not present in sshthresh map
+        ret = bpf_map_update_elem(&SSThreshMap, &key, &val, BPF_NOEXIST);
+        if (ret != 0) bpf_printk("log: SSThresh value update to NOEXIST failed");
+        return 0;
+    }
+
+    // key present in ssthresh map
+    if (!*userSSThresh) return 0;
+    // *userSSThresh = ceil(*userSSThresh/MSS)
+    int newSSThresh = (unsigned long long)(*userSSThresh+MSS-1)/(unsigned long long)MSS;
+    bpf_printk("log: updating SSThresh to %d (%d MSS)", *userSSThresh, newSSThresh);
+    *userSSThresh = newSSThresh;
 
     // setting the initial congestion window to be same as `*userCwnd`
     ret = bpf_setsockopt(skops, SOL_TCP, TCP_BPF_IW, userCwnd, sizeof(int));
-    if (ret != 0) bpf_printk("error %d: failed to set socket cwnd to %d", ret, *userCwnd);
-    else bpf_printk("log: set socket cwnd to %d", *userCwnd);  
+    if (ret != 0) bpf_printk("error %d: failed to set socket CWND to %d", ret, *userCwnd);
+    else bpf_printk("log: set socket CWND to %d", *userCwnd);
+
+    // setting the slow start threshold to be same as `*userSSThresh`
+    // Reference: https://netdevconf.info/2.2/papers/brakmo-tcpbpf-talk.pdf
+    ret = bpf_setsockopt(skops, SOL_TCP, TCP_BPF_SNDCWND_CLAMP, userSSThresh, sizeof(int));
+    if (ret != 0) bpf_printk("error %d: failed to set socket SSThresh to %d", ret, *userSSThresh);
+    else bpf_printk("log: set socket SSThresh to %d", *userSSThresh);
 
     bpf_printk("log: return value %d", ret);
     // passing the return value back to the kernel
--- trace_flush.sh
+++ trace_flush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -xe
 
 # This script flushes the trace_pipe
-
 sudo echo 0 > /sys/kernel/debug/tracing/tracing_on
 sudo echo > /sys/kernel/debug/tracing/trace
 sudo echo 1 > /sys/kernel/debug/tracing/tracing_on
